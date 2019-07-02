7fe60eafbaa99cfb89e1de67017a6e77fe09f5d5
Rada Berar <rada.berar@signify.com>
2019-07-02 11:11:07 +0200

    Build and deploy win batch script

--- QtDesktop/deploy_win.bat
+++ QtDesktop/deploy_win.bat
@@ -1,19 +1,28 @@
-SET QTBINDIR=C:\Qt\5.13.0\msvc2015_64\bin
+SET QTBINDIR=C:\Qt\5.13.0\mingw73_64\bin
+SET COMPILERDIR=C:\Qt\Tools\mingw730_64\bin
 SET QTIFWDIR=C:\Qt\Tools\QtInstallerFramework\3.0\bin
-SET BUILDDIR=release
+set QTINCLUDEDIR=C:\Qt\5.13.0\mingw73_64\include
+set QTLIBSDIR=C:\Qt\5.13.0\mingw73_64\lib
+
 SET APPEXE=OC-Scheduler.exe
 
-rmdir installer /s /q
+set PATH=%PATH%;%QTBINDIR%;%COMPILERDIR%;%QTIFWDIR%;%QTINCLUDEDIR%;%QTLIBSDIR%
+
+xcopy OC-Scheduler\* build\ /s /e
+cd build
+qmake
+mingw32-make release
+
 mkdir data
-mkdir data\languages
 
-copy %BUILDDIR%\release\%APPEXE% data\%APPEXE%
-copy OC-Scheduler\languages data\languages
-copy OC-Scheduler\oc_logo* data
+copy release\%APPEXE% data\%APPEXE%
+xcopy languages\* data\languages\ /s /e
+copy oc_logo* data
 
-%QTBINDIR%/windeployqt.exe data\%APPEXE%
+windeployqt.exe data\%APPEXE%
 
-xcopy OC-Scheduler\installer\* installer\ /s /e
 move data installer\packages\com.ohanacodedev.OCScheduler\ 
 
-%QTIFWDIR%\binarycreator.exe --offline-only -t %QTIFWDIR%\installerbase.exe -p installer\packages -c installer\config\config_win32.xml OC_Scheduler_installer
+binarycreator.exe --offline-only -p installer\packages -c installer\config\config_win32.xml ..\OC_Scheduler_installer
+cd ..
+rmdir build /s /q
