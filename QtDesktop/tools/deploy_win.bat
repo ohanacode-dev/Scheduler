f887209e08fbd5625db5cdbda434a1b68aa52e81
Rada Berar <rada.berar@signify.com>
2019-07-05 12:05:53 +0200

    Fixed windows desktop deploy script

--- QtDesktop/tools/deploy_win.bat
+++ QtDesktop/tools/deploy_win.bat
@@ -1,6 +1,6 @@
 SET QTBINDIR=C:\Qt\5.13.0\mingw73_64\bin
 SET COMPILERDIR=C:\Qt\Tools\mingw730_64\bin
-SET QTIFWDIR=C:\Qt\Tools\QtInstallerFramework\3.0\bin
+SET QTIFWDIR=C:\Qt\Tools\QtInstallerFramework\3.1\bin
 set QTINCLUDEDIR=C:\Qt\5.13.0\mingw73_64\include
 set QTLIBSDIR=C:\Qt\5.13.0\mingw73_64\lib
 
@@ -8,7 +8,8 @@ SET APPEXE=OC-Scheduler.exe
 
 set PATH=%PATH%;%QTBINDIR%;%COMPILERDIR%;%QTIFWDIR%;%QTINCLUDEDIR%;%QTLIBSDIR%
 
-xcopy OC-Scheduler\* build\ /s /e
+rmdir build /s /q
+xcopy ..\OC-Scheduler\* build\ /s /e
 cd build
 qmake
 mingw32-make release
@@ -23,6 +24,6 @@ windeployqt.exe data\%APPEXE%
 
 move data installer\packages\com.ohanacodedev.OCScheduler\ 
 
-binarycreator.exe --offline-only -p installer\packages -c installer\config\config_win32.xml ..\OC_Scheduler_installer
+del ..\..\OC_Scheduler_win_installer*
+binarycreator.exe --offline-only -p installer\packages -c installer\config\config_win32.xml ..\..\OC_Scheduler_win_installer
 cd ..
-rmdir build /s /q
